09eb7349d52ed97dfabc5b8f8e05fa0e5110636e
Vick Fan <vickfct@gmail.com>
2023-07-02 09:54:16 +0800

    FIX: encryption logic and add job key

--- cron_starter/app.js
+++ cron_starter/app.js
@@ -6,25 +6,22 @@ module.exports.lambdaHandler = async (event, context) => {
 
   const secret = process.env.CRON_SECRET
   const iv = process.env.CRON_IV
+  const jobKey = process.env.JOB_KEY
 
   // An encrypt function
   function encrypt(text) {
     // Creating Cipheriv with its parameter
     let cipher = crypto.createCipheriv('aes-256-cbc', secret, iv)
-    let encrypted = cipher.update(text)
-    encrypted = Buffer.concat([encrypted, cipher.final()])
-
-    // Return signature
-    return encrypted.toString('base64')
+    return cipher.update(JSON.stringify(text), 'utf8', 'base64')
   }
 
   const timestampObj = { timestamp: Math.floor(Date.now() / 1000)}
   const signature = encrypt(JSON.stringify(timestampObj))
 
   const res = await fetch(process.env.URL, {
-    method: 'put',
+    method: 'post',
     headers: { "content-type": "application/json", },
-    body: { signature }
+    body: { signature, jobKey }
   })
 
   return res.status
